96607918ba851f21291422d37a19419ffd39426b
dhias ulhaq <dhias.ulhaq30@gmail.com>
2024-03-25 20:59:26 +0700

    topik 1 java

---
+++ PBO/ACT 1/no1.java
@@ -0,0 +1,34 @@
+public class VolumeBola {
+    public static void main(String[] args) {
+
+        double r = 7.5; // Jari-jari bola
+        double volume;  // Untuk menyimpan hasil penghitungan volume bola
+
+        // [1] Hitung volume bola
+        volume = 4.0 / 3 * Math.PI * Math.pow(r, 3);
+
+        // [2] Tampilkan volume bola
+        System.out.printf("Volume bola = %.13f", volume);
+
+
+    }
+
+}
+
+/* 
+    
+JOKI BY RINCEMBER 
+IG : rincemberr 
+
+:D
+
+ */
+ 
+
+ 
+
+
+   
+
+
+ 
