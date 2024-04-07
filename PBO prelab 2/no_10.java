9bef1c50a27f180442ab4a89fc0be0670f3b0428
dhias ulhaq <dhias.ulhaq30@gmail.com>
2024-04-07 08:13:31 +0700

    prelab 2

---
+++ PBO prelab 2/no_10.java
@@ -0,0 +1,17 @@
+public static double readDouble(String prompt) {
+    // Tuliskan kode method readDouble di bawah comment ini.
+    System.out.print(prompt + " ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            return scanner.nextDouble();
+
+        } catch (Exception e) {
+
+            System.out.println("Input yang Anda masukkan tidak valid.");
+
+            return readDouble(prompt); 
+        }
+}
