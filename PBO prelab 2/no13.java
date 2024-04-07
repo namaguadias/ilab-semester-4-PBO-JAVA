e23e24c2b4fbbf6a8d8b0e1774f4c14c4095bc47
dhias ulhaq <dhias.ulhaq30@gmail.com>
2024-04-07 08:45:44 +0700

    prelab 2

---
+++ PBO prelab 2/no13.java
@@ -0,0 +1,26 @@
+public static double minimum(double[] arr) {
+
+    double min = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+
+        if (arr[i] < min) {
+
+            min = arr[i];
+
+        }
+
+    }
+
+    return min;
+
+}
+
+/* 
+    
+JOKI BY RINCEMBER 
+IG : rincemberr 
+
+:D
+
+ */
